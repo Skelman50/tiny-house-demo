c1a16272f835eea67b1713b554bed86cc59aa0c7
Skelman50 <skelman50@gmail.com>
2020-04-20 21:23:53 +0300

    user resolvers

--- server/src/graphql/typeDefs.ts
+++ server/src/graphql/typeDefs.ts
@@ -9,8 +9,60 @@ export const typeDefs = gql`
     didRequest: Boolean!
   }
 
+  type Booking {
+    id: ID!
+    listing: Listing!
+    tenant: User!
+    checkIn: String!
+    checkOut: String!
+  }
+
+  type Bookings {
+    total: Int!
+    result: [Booking!]!
+  }
+
+  enum ListingType {
+    APARTMENT
+    HOUSE
+  }
+
+  type Listing {
+    id: ID!
+    title: String!
+    description: String!
+    image: String!
+    host: User!
+    type: ListingType!
+    address: String!
+    country: String!
+    admin: String!
+    city: String!
+    bookings(limit: Int!, page: Int!): Bookings
+    bookingsIndex: String!
+    price: Int!
+    numOfGuests: Int!
+  }
+
+  type Listings {
+    total: Int!
+    result: [Listing!]!
+  }
+
+  type User {
+    id: ID!
+    name: String!
+    avatar: String!
+    contact: String!
+    hasWallet: Boolean!
+    income: Int
+    bookings(limit: Int!, page: Int!): Bookings
+    listings(limit: Int!, page: Int!): Listings!
+  }
+
   type Query {
     authUrl: String!
+    user(id: ID!): User!
   }
 
   input LoginInput {
